e1e37857bfc6d1d3516ca7568155d6cd1a2c7f61
Andrew Jones <andrewjonesdev1@gmail.com>
2020-02-06 01:08:38 -0600

    Sensors Specific Words

--- MusicConsoleApp/Program.cs
+++ MusicConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace MusicConsoleApp
 {
@@ -14,7 +15,14 @@ namespace MusicConsoleApp
             List<Music> Playlist = JsonConvert.DeserializeObject < List <Music>>(json);
             Playlist.ForEach(x => Console.WriteLine(x.Song));
             Playlist.FindAll(x => x.Artists.Contains("Drake")).ForEach(y=> Console.WriteLine(y.Song + " - " + y.Artists));
-            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("lean")).ForEach(y => Console.WriteLine(y.Song + " - " + y.Artists));
+            Playlist.FindAll(x => x.Lyrics.ToLower().Contains(" lean ")).ForEach(y => Console.WriteLine(y.Song + " - " + y.Artists));
+            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("fuck")).ForEach(y => y.Lyrics=Regex.Replace(y.Lyrics,"fuck", "****", RegexOptions.IgnoreCase));
+            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("nigg")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "nigg", "****", RegexOptions.IgnoreCase));
+            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("bitch")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "bitch", "*****", RegexOptions.IgnoreCase));
+            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("pussy")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "pussy", "*****", RegexOptions.IgnoreCase));
+            Playlist.FindAll(x => x.Lyrics.ToLower().Contains(" ass ")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, " ass ", "***", RegexOptions.IgnoreCase));
+            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("shit")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "shit", "****", RegexOptions.IgnoreCase));
+            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("*")).ForEach(y => Console.WriteLine(y.Song + " - " + y.Artists + "\n" + y.Lyrics));
         }
 
         public class Music
