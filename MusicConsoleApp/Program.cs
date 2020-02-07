2b120bafeed2ebae768595ec4f843c8ff895ec6d
Andrew Jones <andrewjonesdev1@gmail.com>
2020-02-06 23:29:00 -0600

    Added Lambdas to be done on one line and fleshed out non lambda code

--- MusicConsoleApp/Program.cs
+++ MusicConsoleApp/Program.cs
@@ -10,18 +10,14 @@ namespace MusicConsoleApp
     {
         static void Main(string[] args)
         {
-            StreamReader r = new StreamReader("../../../songs.json");
-            string json = r.ReadToEnd();
-            List<Music> Playlist = JsonConvert.DeserializeObject < List <Music>>(json);
-            Playlist.ForEach(x => Console.WriteLine(x.Song));
-            Playlist.FindAll(x => x.Artists.Contains("Drake")).ForEach(y=> Console.WriteLine(y.Song + " - " + y.Artists));
-            Playlist.FindAll(x => x.Lyrics.ToLower().Contains(" lean ")).ForEach(y => Console.WriteLine(y.Song + " - " + y.Artists));
-            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("fuck")).ForEach(y => y.Lyrics=Regex.Replace(y.Lyrics,"fuck", "****", RegexOptions.IgnoreCase));
-            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("nigg")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "nigg", "****", RegexOptions.IgnoreCase));
-            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("bitch")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "bitch", "*****", RegexOptions.IgnoreCase));
-            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("pussy")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "pussy", "*****", RegexOptions.IgnoreCase));
-            Playlist.FindAll(x => x.Lyrics.ToLower().Contains(" ass ")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, " ass ", "***", RegexOptions.IgnoreCase));
-            Playlist.FindAll(x => x.Lyrics.ToLower().Contains("shit")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "shit", "****", RegexOptions.IgnoreCase));
+            StreamReader reader = new StreamReader("../../../Songs.json");
+            string music = reader.ReadToEnd();
+            reader = new StreamReader("../../../BadWords.json");
+            string badWords = reader.ReadToEnd();
+            List<Music> Playlist = JsonConvert.DeserializeObject<List<Music>>(music);
+            List<BadWord> BadWords = JsonConvert.DeserializeObject<List<BadWord>>(badWords);
+
+            BadWords.ForEach(w => Playlist.FindAll(x => x.Lyrics.ToLower().Contains(w.Word)).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, w.Word, Regex.Replace(w.Word, "[^0-9.]", "*"), RegexOptions.IgnoreCase)));
             Playlist.FindAll(x => x.Lyrics.ToLower().Contains("*")).ForEach(y => Console.WriteLine(y.Song + " - " + y.Artists + "\n" + y.Lyrics));
         }
 
@@ -33,5 +29,34 @@ namespace MusicConsoleApp
             public string Lyrics { get; set; }
 
         }
+        public class BadWord
+        {
+            public string Word { get; set; }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Playlist.ForEach(x => Console.WriteLine(x.Song));
+//Playlist.FindAll(x => x.Artists.Contains("Drake")).ForEach(y=> Console.WriteLine(y.Song + " - " + y.Artists));
+//Playlist.FindAll(x => x.Lyrics.ToLower().Contains(" lean ")).ForEach(y => Console.WriteLine(y.Song + " - " + y.Artists));
+//Playlist.FindAll(x => x.Lyrics.ToLower().Contains("fuck")).ForEach(y => y.Lyrics=Regex.Replace(y.Lyrics,"fuck", "****", RegexOptions.IgnoreCase));
+//Playlist.FindAll(x => x.Lyrics.ToLower().Contains("nigg")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "nigg", "****", RegexOptions.IgnoreCase));
+//Playlist.FindAll(x => x.Lyrics.ToLower().Contains("bitch")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "bitch", "*****", RegexOptions.IgnoreCase));
+//Playlist.FindAll(x => x.Lyrics.ToLower().Contains("pussy")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "pussy", "*****", RegexOptions.IgnoreCase));
+//Playlist.FindAll(x => x.Lyrics.ToLower().Contains(" ass ")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, " ass ", "***", RegexOptions.IgnoreCase));
+//Playlist.FindAll(x => x.Lyrics.ToLower().Contains("shit")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "shit", "****", RegexOptions.IgnoreCase));
---
+++ NoLambdas/Program.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace NoLambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader reader = new StreamReader("../../../Songs.json");
+            string music = reader.ReadToEnd();
+            reader = new StreamReader("../../../BadWords.json");
+            string badWords = reader.ReadToEnd();
+            List<Music> Playlist = JsonConvert.DeserializeObject<List<Music>>(music);
+            List<BadWord> BadWords = JsonConvert.DeserializeObject<List<BadWord>>(badWords);
+            string bleep = "";
+
+            //Iterate through each bad word in the list of bad words
+            foreach (BadWord badWord in BadWords)
+            {
+                //Iterate through each song in the playlist
+                foreach (Music song in Playlist)
+                {
+                    //check if lyrics of specific song has specific bad word
+                    if (song.Lyrics.Contains(badWord.Word))
+                    {
+                        //Create a copy of the bad word with a bleeped out word the same length
+                        bleep = Regex.Replace(badWord.Word, "[^0-9.]", "*");
+                        //Within a song's Lyrics replace a Bad Word With the bleeped out word
+                        song.Lyrics = Regex.Replace(song.Lyrics, badWord.Word, bleep, RegexOptions.IgnoreCase);
+                    }
+                }
+            }
+            //Iterate through song in playlist
+            foreach (Music song in Playlist)
+            {
+
+                if (song.Lyrics.Contains("*"))
+                {
+                    //Print song title with artist title and lyrics below
+                    Console.WriteLine(song.Song + " - " + song.Artists + "\n" + song.Lyrics);
+                }
+            }
+
+        }
+        public class Music
+        {
+            public string Artists { get; set; }
+            public string Song { get; set; }
+            public string Album { get; set; }
+            public string Lyrics { get; set; }
+
+        }
+        public class BadWord
+        {
+            public string Word { get; set; }
+        }
+    }
+}
