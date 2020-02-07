84f5373ebe31d0455b034edf2d936f68ee46c9cf
Andrew Jones <andrewjonesdev1@gmail.com>
2020-02-07 00:20:11 -0600

    Removed Commented out Code

--- MusicConsoleApp/Program.cs
+++ MusicConsoleApp/Program.cs
@@ -35,28 +35,3 @@ namespace MusicConsoleApp
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Playlist.ForEach(x => Console.WriteLine(x.Song));
-//Playlist.FindAll(x => x.Artists.Contains("Drake")).ForEach(y=> Console.WriteLine(y.Song + " - " + y.Artists));
-//Playlist.FindAll(x => x.Lyrics.ToLower().Contains(" lean ")).ForEach(y => Console.WriteLine(y.Song + " - " + y.Artists));
-//Playlist.FindAll(x => x.Lyrics.ToLower().Contains("fuck")).ForEach(y => y.Lyrics=Regex.Replace(y.Lyrics,"fuck", "****", RegexOptions.IgnoreCase));
-//Playlist.FindAll(x => x.Lyrics.ToLower().Contains("nigg")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "nigg", "****", RegexOptions.IgnoreCase));
-//Playlist.FindAll(x => x.Lyrics.ToLower().Contains("bitch")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "bitch", "*****", RegexOptions.IgnoreCase));
-//Playlist.FindAll(x => x.Lyrics.ToLower().Contains("pussy")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "pussy", "*****", RegexOptions.IgnoreCase));
-//Playlist.FindAll(x => x.Lyrics.ToLower().Contains(" ass ")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, " ass ", "***", RegexOptions.IgnoreCase));
-//Playlist.FindAll(x => x.Lyrics.ToLower().Contains("shit")).ForEach(y => y.Lyrics = Regex.Replace(y.Lyrics, "shit", "****", RegexOptions.IgnoreCase));
